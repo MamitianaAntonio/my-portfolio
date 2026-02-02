786f7cf02502c635b35d70a2a1be260b748a3d9a
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-02-02 19:36:32 +0300

    section(hero) : implement hero section and profile photo on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Navbar from "./components/layout/Navbar.jsx";
+import Hero from "./components/sections/Hero.jsx";
 
 export const App = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+
+      <p className="text-center text-text-secondary py-8">© 2024 Antonio. Tous droits réservés.</p>
+    </>
   );
 };
