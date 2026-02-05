99d1995233586bae7eda3cce6eb054b5253a8923
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-02-05 22:23:11 +0300

    section(skills): implement skills grid skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/layout/Navbar.jsx";
 import Hero from "./components/sections/Hero.jsx";
+import Skills from "./components/sections/Skills.jsx";
 
 export const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
     </>
   );
 };
